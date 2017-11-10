d29398980892cb67b0175a94cd100381eebe4473
Aman Jadon <amanjadon54@gmail.com>
2017-11-10 09:24:35 +0530

    firts commit using git

---
+++ src/main/java/com/tutorialspoint/test/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.tutorialspoint.test;
+
+import javax.faces.bean.ManagedBean;
+
+//@ManagedBean(name="helloWorld",eager=true)
+public class HelloWorld {
+	 public HelloWorld() {
+	      System.out.println("HelloWorld started!");
+	   }
+		
+	   public String getMessage() {
+	      return "Hello World!";
+	   }
+	   
+	   public String add()
+	   {
+		   return "success";
+	   }
+	
+
+}
---
+++ src/main/java/test/TestController.java
@@ -0,0 +1,22 @@
+package test;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestController extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("js working on or not , i guess js submit has issues");
+		RequestDispatcher rd = req.getRequestDispatcher("Welcome.jsp");
+		rd.forward(req, resp);
+		
+		
+	}
+
+}
